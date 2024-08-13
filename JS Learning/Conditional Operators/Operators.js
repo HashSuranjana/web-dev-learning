d139e6fad8c1071c9071d1ea2eba62ea50332282
Hash <hasithasuranjana12@gmail.com>
2024-08-13 21:40:02 +0530

    comparison operators excercies done

---
+++ JS Learning/Conditional Operators/Operators.js
@@ -0,0 +1,19 @@
+let distance = 40 ;  //declating varible distance
+
+//check the shipping cost and logging it into the console
+if (distance >20) {
+    console.log("Shipping cost is 100 Rs.");
+} else{
+    console.log("Shipping cost is 50 Rs.");
+}
+
+
+const num = 20 ; //declaring the num variable
+
+//checking whether the number's type and value both are equals
+if (num === '20') {
+    document.getElementById('para').innerHTML = '"20" is equal to "20" !';
+}else{
+    document.getElementById('para').innerHTML = '20 is not equal to "20" !';
+}
+
