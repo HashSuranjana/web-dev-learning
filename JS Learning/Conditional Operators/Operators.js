1a5fb175edbf0448473e328b1541f4b09cd47f0c
Hash <hasithasuranjana12@gmail.com>
2024-08-13 22:09:26 +0530

    Logical operators done

--- JS Learning/Conditional Operators/Operators.js
+++ JS Learning/Conditional Operators/Operators.js
@@ -1,3 +1,5 @@
+//Comparison Operators
+
 let distance = 40 ;  //declating varible distance
 
 //check the shipping cost and logging it into the console
@@ -16,4 +18,26 @@ if (num === '20') {
 }else{
     document.getElementById('para').innerHTML = '20 is not equal to "20" !';
 }
+
+//Logical Operators
+
+//decaring an array of objects
+let person = { 
+         name :"Hash",
+        country : "SriLanka",
+        isPremium : false,
+        age :20
+    }
+
+if(person.age>=18 && person.isPremium === true) {
+    document.getElementById('para2').innerHTML = person.name + " can access anything !";
+} else if (person.age <18 || person.isPremium === true){
+    document.getElementById('para2').innerHTML = person.name + " can access anything !";
+}else{
+    document.getElementById('para2').innerHTML = person.name + " can access premium things !";
+}
+
+
+
+
 
