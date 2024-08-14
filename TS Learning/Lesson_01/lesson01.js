e8811e32109940a1009f75d5383928d866aa404e
Hash <hasithasuranjana12@gmail.com>
2024-08-14 11:43:43 +0530

    TS compiling to JS done

---
+++ TS Learning/Lesson_01/lesson01.js
@@ -0,0 +1,6 @@
+var myName = 'Hash';
+console.log(myName);
+var inputs = document.querySelectorAll('input');
+inputs.forEach(function (input) {
+    console.log(input);
+});
---
+++ TS Learning/Lesson_01/lesson01.ts
@@ -0,0 +1,11 @@
+const myName = 'Hash' ;
+
+console.log(myName)
+
+const inputs = document.querySelectorAll('input') ;
+
+inputs.forEach(input => {
+    console.log(input) ;
+})
+
+
