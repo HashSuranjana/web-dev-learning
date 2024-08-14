aa2627b847cf02774365bfa9fc5eecbc4b106144
Hash <hasithasuranjana12@gmail.com>
2024-08-14 15:55:41 +0530

    Dynamic excercises done

---
+++ TS Learning/Dynamic/dynamic.ts
@@ -0,0 +1,18 @@
+let car: any = 'Volvo' ;  //initially car is a string
+
+car  = 25 //now it is a number
+
+
+//objects
+
+let job : {
+    title :any,
+    salary: any
+}
+
+//disadvantages
+job = {
+    title : 25,
+    salary : ''
+}
+
