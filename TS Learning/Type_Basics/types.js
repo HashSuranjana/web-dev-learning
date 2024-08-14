08f696de32bda91718a626eccdefa9dd7abd2f33
Hash <hasithasuranjana12@gmail.com>
2024-08-14 12:39:47 +0530

    primitive types excercises done

---
+++ TS Learning/Type_Basics/types.js
@@ -0,0 +1,8 @@
+//explicit Types
+var name = "Hash"; //Declaring a string variable
+console.log(name);
+var age; // initializing age variable
+age = 10; //declaring age
+console.log(age);
+var isLoggedIn = false; //decalring boolean variable
+console.log(isLoggedIn);
---
+++ TS Learning/Type_Basics/types.ts
@@ -0,0 +1,22 @@
+//explicit Types
+
+let name :string = "Hash" ;  //Declaring a string variable
+
+console.log(name)
+
+let age :number; // initializing age variable
+
+age = 10  //declaring age
+
+console.log(age)
+
+let isLoggedIn : boolean = false //decalring boolean variable
+console.log(isLoggedIn)
+
+
+
+
+
+
+
+
