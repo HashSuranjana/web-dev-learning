6b2710e62373538e43272ea87a3c5f4f09772459
Hash <hasithasuranjana12@gmail.com>
2024-08-14 12:47:36 +0530

    arrays and object types done

--- TS Learning/Type_Basics/types.js
+++ TS Learning/Type_Basics/types.js
@@ -6,3 +6,32 @@ age = 10; //declaring age
 console.log(age);
 var isLoggedIn = false; //decalring boolean variable
 console.log(isLoggedIn);
+//arrays of Strings
+var fruits = ['Apple', 'Banana', 'Mango', 'Orange'];
+fruits.push('Grapes'); //adding another fruit
+//iterate through the fruits and console logging it
+fruits.forEach(function (fruit) {
+    console.log(fruit);
+});
+//arrays of numbers
+var marks = [];
+marks.push(10);
+marks.push(20);
+marks.push(30);
+//filtering marks and logging it into the console
+marks.filter(function (mark) {
+    mark > 10 ? console.log(mark) : "";
+});
+//union arrays 
+var mixedArray = [1, 5, 'hello', 10, 'world'];
+mixedArray[2] = 2;
+mixedArray[0] = 'Josh';
+console.log(mixedArray[2]);
+console.log(mixedArray[1]);
+//Objects 
+var person;
+person = {
+    personName: 'Hash',
+    personAge: 20
+};
+console.log(person.personName + " " + person.personAge);
--- TS Learning/Type_Basics/types.ts
+++ TS Learning/Type_Basics/types.ts
@@ -14,6 +14,54 @@ let isLoggedIn : boolean = false //decalring boolean variable
 console.log(isLoggedIn)
 
 
+//arrays of Strings
+
+let fruits :string[] = ['Apple','Banana','Mango','Orange'];
+
+fruits.push('Grapes'); //adding another fruit
+
+//iterate through the fruits and console logging it
+fruits.forEach(fruit =>{
+    console.log(fruit);
+})
+
+//arrays of numbers
+
+let marks :number[] = [];
+
+marks.push(10);
+marks.push(20);
+marks.push(30);
+
+//filtering marks and logging it into the console
+marks.filter(mark => {
+    mark >10 ? console.log(mark) : ""
+})
+
+//union arrays 
+
+let mixedArray: (string|number)[] = [1,5,'hello',10,'world'] ;
+
+mixedArray[2] = 2
+mixedArray[0] = 'Josh'
+
+console.log(mixedArray[2])
+console.log(mixedArray[1])
+
+//Objects 
+
+let person : {
+    personName : string,
+    personAge :number,
+    personCountry? :string //this is optional 
+}
+
+person = {
+    personName :'Hash',
+    personAge : 20
+}
+
+console.log(person.personName + " " + person.personAge)
 
 
 
